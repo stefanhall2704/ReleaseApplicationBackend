ecff633d8ce98464ea7458b329f1818befd24cbb
Stefan Hall <stefanhall@SHAL003975.attlocal.net>
2023-02-03 23:28:52 -0500

    Move API's off main and into its own module

--- src/models.rs
+++ src/models.rs
@@ -1308,7 +1308,7 @@ pub struct VstsFeatureCompliance {
 }
 
 
-#[derive(Insertable, Serialize, Serialize)]
+#[derive(Insertable, Serialize)]
 #[diesel(table_name=vsts_feature_compliance)]
 pub struct NewVstsFeatureCompliance {
     pub FeatureID: i32,
