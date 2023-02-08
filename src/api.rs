55841c4a059129e7e12620adcbca0788bdb0bc1d
Stefan Hall <stefanhall@SHAL003975.attlocal.net>
2023-02-07 21:19:29 -0500

    Releases API's

--- src/api.rs
+++ src/api.rs
@@ -228,3 +228,123 @@ pub fn delete_user(id: i32, json: Json<JsonValue>) -> Result<std::string::String
     let response = format!("User deleted in database by id: {}", id);
     Ok(response)
 }
+
+#[post("/api/createRelease", format = "application/json", data = "<json>")]
+pub fn create_release(json: Json<JsonValue>) -> Json<JsonValue> {
+    let connection = &mut establish_connection();
+
+    let data_string = json.to_string();
+    let data: &str = &data_string;
+    let v: Value = serde_json::from_str(data).unwrap();
+
+    let name = v["name"].as_str().unwrap().to_owned();
+    let release_date = NaiveDate::from_ymd_opt(2023, 2, 5)
+        .unwrap()
+        .and_hms_opt(9, 10, 11)
+        .unwrap();
+    let is_off_cycle = v["is_off_cycle"].as_bool().unwrap();
+    let release_status_id: i32 = v["release_status_id"].as_i64().unwrap() as i32;
+    let downtime_notes = v["downtime_notes"].as_str().unwrap().to_owned();
+    let release_commit_date = v["release_commit_date"].as_str().unwrap().to_owned();
+    let regression_query_link = v["regression_query_link"].as_str().unwrap().to_owned();
+    let description = v["description"].as_str().unwrap().to_owned();
+    let change_control_number = v["change_control_number"].as_str().unwrap().to_owned();
+    let total_work_items_tagged_for_release: i32 =
+        v["total_work_items_tagged_for_release"].as_i64().unwrap() as i32;
+    let is_ready_for_qa = v["is_ready_for_qa"].as_bool().unwrap();
+
+    create_db_release(
+        connection,
+        name,
+        release_date,
+        Some(is_off_cycle),
+        Some(release_status_id),
+        Some(downtime_notes),
+        release_commit_date,
+        Some(regression_query_link),
+        Some(description),
+        Some(change_control_number),
+        Some(total_work_items_tagged_for_release),
+        Some(is_ready_for_qa),
+    );
+
+    let response = json!({
+        "received": json.into_inner(),
+        "message": format!("Team created in database")
+    });
+
+    Json(response)
+}
+
+#[get("/api/getRelease/<id>")]
+pub fn get_release(id: i32) -> Result<std::string::String, ()> {
+    let release = get_db_release_by_id(id).unwrap();
+    let user_json = to_string(&release).unwrap();
+    Ok(user_json)
+}
+
+#[post(
+    "/api/updateRelease/<id>",
+    format = "application/json",
+    data = "<json>"
+)]
+pub fn update_release(id: i32, json: Json<JsonValue>) -> Json<JsonValue> {
+    let connection = &mut establish_connection();
+
+    let data_string = json.to_string();
+    let data: &str = &data_string;
+    let v: Value = serde_json::from_str(data).unwrap();
+
+    let name = v["name"].as_str().unwrap().to_owned();
+    let release_date = NaiveDate::from_ymd_opt(2023, 2, 5)
+        .unwrap()
+        .and_hms_opt(9, 10, 11)
+        .unwrap();
+    let is_off_cycle = v["is_off_cycle"].as_bool().unwrap();
+    let release_status_id: i32 = v["release_status_id"].as_i64().unwrap() as i32;
+    let downtime_notes = v["downtime_notes"].as_str().unwrap().to_owned();
+    let release_commit_date = v["release_commit_date"].as_str().unwrap().to_owned();
+    let regression_query_link = v["regression_query_link"].as_str().unwrap().to_owned();
+    let description = v["description"].as_str().unwrap().to_owned();
+    let change_control_number = v["change_control_number"].as_str().unwrap().to_owned();
+    let total_work_items_tagged_for_release: i32 =
+        v["total_work_items_tagged_for_release"].as_i64().unwrap() as i32;
+    let is_ready_for_qa = v["is_ready_for_qa"].as_bool().unwrap();
+
+    update_db_release(
+        connection,
+        id,
+        name,
+        release_date,
+        Some(is_off_cycle),
+        Some(release_status_id),
+        Some(downtime_notes),
+        release_commit_date,
+        Some(regression_query_link),
+        Some(description),
+        Some(change_control_number),
+        Some(total_work_items_tagged_for_release),
+        Some(is_ready_for_qa),
+    );
+
+    let response = json!({
+        "received": json.into_inner(),
+        "message": format!("Team updated in database")
+    });
+
+    Json(response)
+}
+
+#[delete(
+    "/api/deleteRelease/<id>",
+    format = "application/json",
+    data = "<json>"
+)]
+pub fn delete_release(id: i32, json: Json<JsonValue>) -> Result<std::string::String, ()> {
+    //required print statement
+    println!("{}", json.to_string());
+    let connection = &mut establish_connection();
+    delete_db_release(connection, id);
+    let response = format!("Team deleted in database by id: {}", id);
+    Ok(response)
+}
